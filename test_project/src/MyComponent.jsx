0fb10afbfa545edf922281cbe6797b4d951c995f
inyoung0306 <113179518+inyoung0306@users.noreply.github.com>
2023-01-29 15:46:30 +0900

    Delete test_project directory

--- test_project/src/MyComponent.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import { Component } from 'react';
-
-
-
-const MyComponent = (props) => {
-    return <div>{props.name}의 새롭고 멋진 컴포넌트 </div>
-  };
-  export default MyComponent;
