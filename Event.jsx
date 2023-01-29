3d6efaa806bca0c34e979b1e6c933789127d98df
inyoung0306 <113179518+inyoung0306@users.noreply.github.com>
2023-01-29 16:02:00 +0900

    Add files via upload

---
+++ Event.jsx
@@ -0,0 +1,44 @@
+import React, { Component, useState } from 'react';
+
+const Event = () =>{
+
+    const message = useState('');
+    const onClickEnter = (a) => alert(a)
+    return (
+       <div> <div>
+            <input
+                style = {{height: '50px'}}
+                type = "text"
+                placeholder='ID'
+
+            />
+  
+            
+            <button
+            style={{width:'150px'}}
+                onClick={
+                (e) => {
+                    alert({message});
+            }}>
+                로그인하기
+            </button>
+        </div>
+        <input
+                style = {{height: '50px'}}
+                type = "text"
+                placeholder='password'
+
+            />
+             <button
+                style={{width:'150px'}}
+                onClick={
+                (e) => {
+                    alert({message});
+            }}>
+                회원가입
+            </button>
+        </div>
+    );
+};
+
+export default Event;
