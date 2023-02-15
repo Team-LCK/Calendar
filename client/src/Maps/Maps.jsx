5c11f3020c1f2bcfeb69df40cb24e2c735a4ccd3
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-02-15 14:35:35 +0900

    카카오 지도 마커

--- client/src/Maps/Maps.jsx
+++ client/src/Maps/Maps.jsx
@@ -1,4 +1,4 @@
-import {Map} from "react-kakao-maps-sdk";
+import {Map, MapMarker} from "react-kakao-maps-sdk";
 import React from "react";
 
 function Maps(){
@@ -6,8 +6,15 @@ function Maps(){
         <Map
           center={{lat:37.558090961074825, lng:126.99847210567884 }} 
           style ={{width: "500px", height: "500px"}} >
+            
+        <MapMarker
+          position ={{lat: 37.558090961074825, lng:126.99847210567884}}
+        >
+        <div style={{color: "#000"}}>Hello!</div>  
+        </MapMarker>
 
         </Map>
+
     )
 }
 
